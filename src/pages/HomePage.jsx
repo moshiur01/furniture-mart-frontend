7c3ef685ee9a89415eef9207e9b2dc20646bca7a
moshiur01 <mr.moshiur314@gmail.com>
2024-06-04 14:27:12 +0600

    [DONE] feature card added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import FeatureCard from "../components/FeatureCard";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <FeatureCard />
     </div>
   );
 };
